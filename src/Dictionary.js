9ff5f0d972e5d895e724c94d64acb74795c92c24
slater19 <sachinaggarwal19@gmail.com>
2025-01-13 17:13:33 +0530

    my change1

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -35,22 +35,22 @@ const Dictionary = () => {
   const filtered = response.filter(response => response.word.includes(inputValue));
 console.log(filtered)
   return (
-    <div style={{fontWeight:"bold",textAlign: 'center',}}>
-      <div style={{fontWeight:"bold",textAlign: 'center',}}>
-        <h1 style={{fontWeight:"bold",textAlign: 'center',}}> Dictionary App</h1>
-        {/* <p className="text-center mt-1 mb-10 text-slate-300 text-lg">Find definisions for word</p> */}
+    <div >
+      <div >
+        <h1 style={{fontWeight:"bold"}}> Dictionary App</h1>
+        
       
-        <div style={{fontWeight:"bold",textAlign: 'center',}}className="flex items-center justify-center mt-5">
-          <div style={{fontWeight:"bold",textAlign: 'center',}}className="flex border-2 border-gray-200 rounded">
-            <input style={{fontWeight:"bold",}} className="px-4 py-2 md:w-80" type="text" placeholder="Search for a word.." onChange={handleInputChange} value={value} onKeyDown={handleInputKeyDown} />
-            <button style={{fontWeight:"bold",textAlign: 'center',}} className="bg-blue-400 border-l px-4 py-2 text-white" onClick={handleSubmit}>Search</button>
-          </div>
-        </div>
-   <p style={{fontWeight:"bold",textAlign: 'center',}}className="font-bold text-center mt-1 mb-10 text-slate-300 text-lg">Definition</p>
-       <br/>
-        { inputValue ?
+        
+          
+            <input type="text" placeholder="Search for a word.." onChange={handleInputChange} value={value} onKeyDown={handleInputKeyDown} />
+            <button style={{fontWeight:"bold"}}  onClick={handleSubmit}>Search</button>
+          
+        
+   <p style={{fontWeight:"bold",}}>Definition:</p>
+       
+        { inputValue?
            <MeanigList mean={filtered}  />
-        :<div> Word not found in the dictionary </div> }
+        :<p> Word not found in the dictionary </p> }
       </div>
     </div>
   );
